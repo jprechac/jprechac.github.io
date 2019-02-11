5493da6c9376d1642e13c88f498191f78304994e
jprechac <jprechac@gmail.com>
2019-02-11 17:56:19 -0600

    big updates, but not really

--- java.js
+++ java.js
@@ -1,1 +1,6 @@
-alert('This webpage is not complete. Please excuse the work in progress.')
+window.setTimeout(
+  alert(
+    'This webpage is not complete. Please excuse the work in progress.',
+    60000
+  )
+)
