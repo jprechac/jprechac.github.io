817e8f56c99a8d1a39beef7b9285c02f969005ce
jprechac <jprech4@lsu.edu>
2019-02-13 10:52:05 -0600

    major javascript changes

--- jordan_resume.js
+++ jordan_resume.js
@@ -1,3 +1,10 @@
 function hide(id) {
-  document.getElementById(id).style.visibility="hidden";
+  var head;
+  if (document.getElementById(id).style.visibility == "visible") {
+    document.getElementById(id).style.visibility = "hidden";
+    head = id + "_head";
+    document.getElementById(head).style.visibility = "visible";
+  } else {
+    document.getElementById(id).style.visibility = "visible";
+  }
 }
