029a64e6e14cefa057107ddfac8f3df62ba88f66
P00327164 <Jordan.Prechac@la.gov>
2019-11-12 12:17:42 -0600

    consistent navbar and delete contact stuff

--- resources/js/events.js
+++ resources/js/events.js
@@ -1,4 +1,4 @@
 window.onload = () => {
-    offsetNavHeight();
+    RunNav();
     FooterYear();
 };
--- resources/js/nav-extra.js
+++ resources/js/nav-extra.js
@@ -1,5 +1,39 @@
-function offsetNavHeight() {
+function RunNav() {
+    SetNav();
+    OffsetNavHeight();
+    NavActive();
+}
+
+function SetNav() {
+    const NavHTML = `
+        <nav class="navbar navbar-dark bg-dark navbar-expand-sm fixed-top" id="nav">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">Jordan Prechac</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <div class="navbar-nav">
+                        <a class="nav-link" id="resume-link" href="resume.html">Resume</a>
+                        <a class="nav-link" id="linkedin-link" href="https://www.linkedin.com/in/jordanprechac/" title="LinkedIn"
+                            target="_blank">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    `;
+    document.getElementById('nav-import').innerHTML = NavHTML;
+};
+
+function OffsetNavHeight() {
     let navHeight = document.getElementById("nav").offsetHeight * 1.1;
     navheight = Math.ceil(navHeight).toString();
     document.body.style.marginTop = navHeight + "px";
+};
+
+function NavActive() {
+    if (document.title == "Jordan Prechac Resume") {
+        document.getElementById("resume-link").classList.add("active");
+    };
 };
